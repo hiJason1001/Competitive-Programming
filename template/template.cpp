41e6dcef57a48362d84edd1509583eecec8b08a1
hiJason1001 <43371677+hiJason1001@users.noreply.github.com>
2025-09-20 19:40:23 +0930

    Add files via upload

---
+++ template/template.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+constexpr ll MOD = 1e9 + 7;
+
+void solve() {
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
+
+
