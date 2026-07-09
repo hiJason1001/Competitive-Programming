97d7664b267607acd5ba05129341d0f29bec69a3
hiJason1001 <43371677+hiJason1001@users.noreply.github.com>
2026-07-09 16:31:21 +0930

    Enhance segment tree, add Union Find and Binary Lifting
    Updated segment tree operations to support lazy propagation and modified binary exponentiation for modular arithmetic. Added Union Find and Binary Lifting classes with their respective methods.

--- template.cpp
+++ template.cpp
@@ -18,7 +18,9 @@ int main() {
 
 
 
-
+/*
+Segment Tree (sum)
+*/
 class SegTree {
 private:
     ll n;
@@ -30,10 +32,10 @@ public:
 
     void propagate(ll l, ll r, ll i) {
         if (lazy[i] != 0) {
-            st[i] = lazy[i];
+            st[i] += (r - l + 1) * lazy[i];
             if (l != r) {
-                lazy[2 * i + 1] = lazy[i];
-                lazy[2 * i + 2] = lazy[i];
+                lazy[2 * i + 1] += lazy[i];
+                lazy[2 * i + 2] += lazy[i];
             }
             lazy[i] = 0;
         }
@@ -42,8 +44,8 @@ public:
     void update(ll l, ll r, ll i, ll left, ll right, ll val) {
         propagate(l, r, i);
         if (left <= l && right >= r) {
-            lazy[i] = val;
-            st[i] = val;
+            lazy[i] += val;
+            propagate(l, r, i);
             return;
         } else if (left > r || right < l) {
             return;
@@ -51,7 +53,7 @@ public:
         ll mid = l + (r - l) / 2;
         update(l, mid, 2 * i + 1, left, right, val);
         update(mid + 1, r, 2 * i + 2, left, right, val);
-        st[i] = max(st[2 * i + 1], st[2 * i + 2]);
+        st[i] = st[2 * i + 1] + st[2 * i + 2];
     }
 
     ll query(ll l, ll r, ll i, ll left, ll right) {
@@ -62,19 +64,13 @@ public:
             return 0;
         }
         ll mid = l + (r - l) / 2;
-        return max(query(l, mid, 2 * i + 1, left, right),
-                   query(mid + 1, r, 2 * i + 2, left, right));
+        return query(l, mid, 2 * i + 1, left, right) + query(mid + 1, r, 2 * i + 2, left, right);
     }
 };
 
-
-
 /*
-
 Binary Trie
-
 */
-
 class Trie {
 private:
     
@@ -111,14 +107,9 @@ public:
     }
 };
 
-
-
 /*
-
 Fraction and fraction hash
-
 */
-
 struct Frac {
     ll numer;
     ll denom;
@@ -187,21 +178,141 @@ struct FracHash {
 };
 
 /*
-
 binary exponentiation
-
 */
-
 ll modpow(ll a, ll b) {
     ll res = 1;
     while (b > 0) {
         if (b & 1)
-            res = res * a;
-        a = a * a;
+            res = (res * a) % MOD;
+        a = (a * a) % MOD;
         b >>= 1;
     }
     return res;
 }
+
+
+/*
+Union Find
+*/
+class UnionFind {
+public:
+    UnionFind(int size) : root(size), rank(size) {
+        for (int i = 0; i < size; i++) {
+            root[i] = i;
+            rank[i] = 1;
+        }
+    }
+
+    int find(int x) {
+        if (x == root[x]) {
+            return x;
+        }
+        return root[x] = find(root[x]);
+    }
+
+    void unionSet(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            if (rank[rootX] > rank[rootY]) {
+                root[rootY] = root[rootX];
+            } else if (rank[rootX] < rank[rootY]) {
+                root[rootX] = root[rootY];
+            } else {
+                root[rootY] = root[rootX];
+                rank[rootX] += 1;
+            }
+        }
+    }
+
+    bool isConnected(int x, int y) {
+        return find(x) == find(y);
+    }
+private:
+    vector<int> root;
+    vector<int> rank;
+};
+
+
+/*
+Binary Lifting
+*/
+class BinLift {
+private:
+    int n, l;
+    vector<vector<int>> adj;
+
+    int timer;
+    vector<int> tin, tout;
+    vector<vector<int>> up;
+
+    void dfs(int v, int p)
+    {
+        tin[v] = ++timer;
+        up[v][0] = p;
+        for (int i = 1; i <= l; ++i)
+            up[v][i] = up[up[v][i-1]][i-1];
+
+        for (int u : adj[v]) {
+            if (u != p)
+                dfs(u, v);
+        }
+
+        tout[v] = ++timer;
+    }
+
+public:
+    BinLift(int n, vector<vector<int>>& graph) : n(n), adj(graph) {}
+
+    bool is_ancestor(int u, int v)
+    {
+        return tin[u] <= tin[v] && tout[u] >= tout[v];
+    }
+
+    int lca(int u, int v)
+    {
+        if (is_ancestor(u, v))
+            return u;
+        if (is_ancestor(v, u))
+            return v;
+        for (int i = l; i >= 0; --i) {
+            if (!is_ancestor(up[u][i], v))
+                u = up[u][i];
+        }
+        return up[u][0];
+    }
+
+    void preprocess(int root) {
+        tin.resize(n);
+        tout.resize(n);
+        timer = 0;
+        l = ceil(log2(n));
+        up.assign(n, vector<int>(l + 1));
+        dfs(root, root);
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
